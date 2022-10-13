18e164d3fb669fc611244f585231132f704c6ce3
Isaac Levy <isaa0267@stud.kea.dk>
2022-10-13 11:37:44 +0200

    added println message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("Hey isaac you dirty little whore! ");
     }
 }
 
